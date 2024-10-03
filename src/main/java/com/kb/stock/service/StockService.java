92c5aab2d2e4fed92e4b87f8ac4496300303c2cf
minju <betaa0528@gmail.com>
2024-10-03 20:23:41 +0900

    #15 - 선생님 페이지의 주식관리의 비즈니스 로직을 작성함 / 박민주

--- src/main/java/com/kb/stock/service/StockService.java
+++ src/main/java/com/kb/stock/service/StockService.java
@@ -4,10 +4,7 @@ import com.kb.stock.domain.HoldingStock;
 import com.kb.stock.domain.RateHistory;
 import com.kb.stock.domain.StockNews;
 import com.kb.stock.domain.StockTrade;
-import com.kb.stock.dto.HoldingStockDTO;
-import com.kb.stock.dto.RateHistoryDTO;
-import com.kb.stock.dto.StockNewsRequest;
-import com.kb.stock.dto.StockTradeRequest;
+import com.kb.stock.dto.*;
 import com.kb.stock.mapper.StockMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j;
@@ -17,6 +14,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.NoSuchElementException;
+import java.util.Optional;
 
 @Log4j
 @RequiredArgsConstructor
@@ -109,6 +107,31 @@ public class StockService {
         stockMapper.insertStockNews(request);
 
         StockNews stockNews = getStockNewsList().get(0);
+        return stockNews;
+    }
+
+    public List<ChartDataDTO> getChartDataDTO() {
+        return stockMapper.selectChartData();
+    }
+
+    public StockChartDTO getStockChartDTO() {
+        StockChartDTO stockChartDTO = stockMapper.selectStockChart();
+        stockChartDTO.setChartData(getChartDataDTO());
+        return stockChartDTO;
+    }
+
+    public StockNews getStockNewsById(long id) {
+        StockNews stockNews = stockMapper.selectStockNewsById(id);
+        return Optional.of(stockNews).orElseThrow(NoSuchElementException::new);
+    }
+
+    public StockNews deleteNews(long newsId) {
+        StockNews stockNews = getStockNewsById(newsId);
+        int result = stockMapper.deleteStockNews(newsId);
+        if(result != 1) {
+            throw new NoSuchElementException();
+        }
+
         return stockNews;
     }
 }
