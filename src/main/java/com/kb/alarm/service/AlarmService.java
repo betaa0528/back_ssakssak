bd62111711524a70491f6b6a2e97dae5b9b950c8
minjoo <betaa0528@gmail.com>
2024-10-07 17:54:34 +0900

    #37 - 알람에 대한 비즈니스 로직 작성 / 박민주

--- src/main/java/com/kb/alarm/service/AlarmService.java
+++ src/main/java/com/kb/alarm/service/AlarmService.java
@@ -1,7 +1,93 @@
 package com.kb.alarm.service;
 
+import com.kb.alarm.dto.Alarm;
+import com.kb.alarm.dto.TeacherProfile;
+import com.kb.alarm.mapper.AlarmMapper;
+import com.kb.student.domain.Student;
+import com.kb.student.mapper.StudentMapper;
+import com.kb.teacher.dto.TeacherDTO;
+import com.kb.teacher.mapper.TeacherMapper;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.NoSuchElementException;
+import java.util.concurrent.ConcurrentHashMap;
+
+@RequiredArgsConstructor
+@Log4j
 @Service
 public class AlarmService {
+
+    private static final String ALARM_NAME = "alarm";
+
+    private final Map<Long, List<SseEmitter>> emitters = new ConcurrentHashMap<>();
+    private final AlarmMapper alarmMapper;
+    private static final Long DEFAULT_TIMEOUT = 60L * 1000 * 60;
+    private final StudentMapper studentMapper;
+    private final TeacherMapper teacherMapper;
+
+    public SseEmitter addEmitter(long tchId) throws IOException {
+        SseEmitter sseEmitter = new SseEmitter(DEFAULT_TIMEOUT);
+        emitters.computeIfAbsent(tchId, k -> new ArrayList<>()).add(sseEmitter);
+        sseEmitter.onCompletion(() -> emitters.get(tchId).remove(sseEmitter));
+        sseEmitter.onTimeout(() -> emitters.get(tchId).remove(sseEmitter));
+
+        try {
+            log.info("send");
+            sseEmitter.send(SseEmitter.event()
+                    .id("id")
+                    .name(ALARM_NAME)
+                    .data("connect completed"));
+        } catch (IOException e) {
+            throw new IOException("알람 연결 에러입니다.");
+        }
+        return sseEmitter;
+    }
+
+    public void sendAlarm(Long userId, String message) {
+        Alarm alarm = new Alarm();
+        Student student = studentMapper.selectStudentById(userId);
+        alarm.setStudent(student);
+        alarm.setType(message);
+        alarm.setTargetId(student.getStdId());
+        alarmMapper.insertAlarm(alarm);
+
+        List<SseEmitter> userEmitters = emitters.get(userId);
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(new MediaType("text", "event-stream", StandardCharsets.UTF_8));
+        if (userEmitters != null) {
+            for(SseEmitter emitter : userEmitters) {
+                try {
+                    emitter.send(SseEmitter.event()
+                            .name(ALARM_NAME)
+                            .data(message, MediaType.TEXT_EVENT_STREAM));
+                } catch (IOException e) {
+                    emitter.completeWithError(e);
+                }
+            }
+        }
+    }
+
+    public List<Alarm> getAlarmByTeacherProfile(String username) {
+        TeacherDTO teacherDTO = teacherMapper.selectByTeacherProfile(username);
+        return alarmMapper.selectAllAlarmListByTeacherId(teacherDTO.getTchId());
+    }
+
+    public void changeIsChecked(long id) {
+        int result = alarmMapper.updateAlarmIsChecked(id);
+        if(result != 1) {
+            throw new NoSuchElementException("update Failed");
+        }
+    }
 }
--- src/main/java/com/kb/coupon/service/CouponService.java
+++ src/main/java/com/kb/coupon/service/CouponService.java
@@ -1,7 +1,11 @@
 package com.kb.coupon.service;
 
+import com.kb.alarm.service.AlarmService;
 import com.kb.coupon.dto.CouponDTO;
 import com.kb.coupon.mapper.CouponMapper;
+import com.kb.member.dto.Member;
+import com.kb.student.domain.Student;
+import com.kb.student.mapper.StudentMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j;
 import org.springframework.context.annotation.PropertySource;
@@ -15,19 +19,26 @@ import java.util.List;
 @PropertySource({"classpath:/application.properties"})
 public class CouponService {
 
-    private final CouponMapper mapper;
+    private final CouponMapper couponMapper;
+    private final StudentMapper studentMapper;
+    private final AlarmService alarmService;
 
     public List<CouponDTO> getAllCoupons() {
-        List<CouponDTO> coupons = mapper.selectCoupon();
+        List<CouponDTO> coupons = couponMapper.selectCoupon();
         return coupons;
     }
 
     public CouponDTO getCouponById(Long id) {
-        CouponDTO coupon = mapper.selectCouponById(id);
+        CouponDTO coupon = couponMapper.selectCouponById(id);
         return coupon;
     }
 
-    public void buyCoupon(Long couponId) {
-        int result = mapper.updateCouponQuantity(couponId);
+    public void buyCoupon(Long couponId, Member member) {
+        Student student = studentMapper.selectStudentByUsernameAndStdName(member.getUsername(), member.getName());
+        StringBuilder sb = new StringBuilder();
+        CouponDTO couponDTO = couponMapper.selectCouponById(couponId);
+        sb.append(member.getName()).append("학생이 ").append(couponDTO.getCpName()).append("을 구매했어요");
+        alarmService.sendAlarm(student.getTchId(), sb.toString());
+        int result = couponMapper.updateCouponQuantity(couponId);
     }
 }
