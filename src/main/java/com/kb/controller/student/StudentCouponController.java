9fbff06a401556d467c688b0cc0412f070903a68
minjoo <betaa0528@gmail.com>
2024-10-07 17:54:58 +0900

    #37 - 알람에 대한 컨트롤러 작성 / 박민주

--- src/main/java/com/kb/controller/student/StudentCouponController.java
+++ src/main/java/com/kb/controller/student/StudentCouponController.java
@@ -2,11 +2,14 @@ package com.kb.controller.student;
 
 import com.kb.coupon.dto.CouponDTO;
 import com.kb.coupon.service.CouponService;
+import com.kb.member.dto.Member;
 import io.swagger.annotations.Api;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -34,8 +37,11 @@ public class StudentCouponController {
     }
 
     @PostMapping("/buy/{couponId}")
-    public ResponseEntity<String> buyCoupon(@PathVariable Long couponId) {
-        couponService.buyCoupon(couponId);
+    public ResponseEntity<String> buyCoupon(@PathVariable Long couponId, @AuthenticationPrincipal Member principal) {
+
+        couponService.buyCoupon(couponId, principal);
+
+//        long tchId = couponService.get
         return ResponseEntity.ok("Coupon purchased successfully!");
     }
 }
--- src/main/java/com/kb/controller/teacher/AlarmController.java
+++ src/main/java/com/kb/controller/teacher/AlarmController.java
@@ -1,30 +1,46 @@
 package com.kb.controller.teacher;
 
+import com.kb.alarm.dto.Alarm;
+import com.kb.alarm.dto.TeacherProfile;
 import com.kb.alarm.service.AlarmService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
+import java.io.IOException;
+import java.util.List;
+
 @RestController
 @RequiredArgsConstructor
+@Slf4j
 @RequestMapping("/api/alarm")
 public class AlarmController {
 
     private final AlarmService alarmService;
 
-    @GetMapping("/subscribe/")
-    public SseEmitter subscribe(Authentication authentication) {
-        SseEmitter emitter = new SseEmitter();
-        authentication.getPrincipal();
-        alarmService.addEmitter(, emitter);
-        return emitter;
+    @GetMapping("/subscribe/{userId}")
+    public SseEmitter subscribe(@PathVariable("userId") long tchId) throws IOException {
+        log.info("subscribe");
+        return alarmService.addEmitter(tchId);
     }
 
     @PostMapping("/send/{userId}")
     public ResponseEntity<String> sendNotification(@PathVariable Long userId, @RequestBody String message) {
-        alarmService.sendNotification(userId, message);
+        alarmService.sendAlarm(userId, message);
         return ResponseEntity.ok("Notification sent");
+    }
+
+    @GetMapping("/history")
+    public ResponseEntity<List<Alarm>> getAlarmHistory(@RequestParam String username) {
+        List<Alarm> alarmList = alarmService.getAlarmByTeacherProfile(username);
+        return ResponseEntity.ok(alarmList);
+    }
+
+    @PostMapping("/checked/{id}")
+    public ResponseEntity<String> udpateAlarmIsChecked(@PathVariable long id) {
+        alarmService.changeIsChecked(id);
+        return ResponseEntity.ok("Alarm check success");
     }
 }
