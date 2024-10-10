62547d202b671b6fe49b555c4bda70cf326087b8
minju <betaa0528@gmail.com>
2024-10-11 00:49:32 +0900

    #63 - 에러 메시지를 원하는걸 보내고 싶어서 핸들러를 작성해주었다. / 박민주

--- src/main/java/com/kb/exception/GlobalExceptionHandler.java
+++ src/main/java/com/kb/exception/GlobalExceptionHandler.java
@@ -1,8 +1,11 @@
 package com.kb.exception;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.NoSuchElementException;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -11,4 +14,10 @@ public class GlobalExceptionHandler {
     public ResponseEntity<String> handleIllegalArgumentException(IllegalArgumentException exception) {
         return ResponseEntity.badRequest().body(exception.getMessage());
     }
+
+    @ExceptionHandler(NoSuchElementException.class)
+    public ResponseEntity<String> handleNoSuchElementException(NoSuchElementException exception) {
+        return ResponseEntity.badRequest().body(exception.getMessage());
+    }
+
 }
