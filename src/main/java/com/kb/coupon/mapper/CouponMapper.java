2a41f0c164820aa6182f01b274f2a4552c63f9e8
minju <betaa0528@gmail.com>
2024-10-11 00:47:05 +0900

    #63 - 학생의 쿠폰 구매와 관련된 매퍼들을 작성했다 / 박민주

--- src/main/java/com/kb/coupon/mapper/CouponMapper.java
+++ src/main/java/com/kb/coupon/mapper/CouponMapper.java
@@ -1,12 +1,24 @@
 package com.kb.coupon.mapper;
 
+import com.kb.coupon.domain.StudentCoupon;
+import com.kb.coupon.dto.BuyRequest;
 import com.kb.coupon.dto.CouponDTO;
+import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
 
 public interface CouponMapper {
     List<CouponDTO> selectCoupon();
+
     CouponDTO selectAvailableCouponById(Long id);
+
     CouponDTO selectCouponById(Long id);
-    int updateCouponQuantity(Long couponId);
+
+    int updateCouponQuantity(BuyRequest request);
+
+    int insertStudentCoupon(@Param("stdId") long stdId, @Param("cpId") long cpId, @Param("quantity") int quantity);
+
+    StudentCoupon selectStudentCoupon(@Param("stdId") long stdId, @Param("cpId") long cpId);
+
+    void updateStudentCoupon(@Param("stdId") long stdId, @Param("cpId") long cpId, @Param("quantity") int quantity);
 }
