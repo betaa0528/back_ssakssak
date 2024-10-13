99410b59712f762f6d0544ad25e49a9aca5c1c66
minju <betaa0528@gmail.com>
2024-10-14 01:35:41 +0900

    #86 - 은행관리에서 적금기능에 대한 데이터를 불러오고 저장하는 매퍼를 작성함 / 박민주

--- src/main/java/com/kb/saving/mapper/SavingMapper.java
+++ src/main/java/com/kb/saving/mapper/SavingMapper.java
@@ -1,9 +1,15 @@
 package com.kb.saving.mapper;
 
+import com.kb.saving.domain.Saving;
+import com.kb.saving.dto.SavingAddPrimeRateDTO;
 import com.kb.saving.dto.SavingDTO;
 
 import java.util.List;
 
 public interface SavingMapper {
     List<SavingDTO> selectSaving();
+
+    List<SavingAddPrimeRateDTO> selectAllSaving();
+
+    int insertSaving(Saving saving);
 }
