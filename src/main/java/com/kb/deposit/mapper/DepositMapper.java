375cde3c98a936ac4a8656ee50c9c3d86b816adf
minju <betaa0528@gmail.com>
2024-10-14 19:05:40 +0900

    #86 - 선생님 페이지에서 은행관리하는 부분의 매퍼에서 에금을 등록하는 쿼리를 추가적으로 작성함 / 박민주

--- src/main/java/com/kb/deposit/mapper/DepositMapper.java
+++ src/main/java/com/kb/deposit/mapper/DepositMapper.java
@@ -1,5 +1,6 @@
 package com.kb.deposit.mapper;
 
+import com.kb.deposit.domain.Deposit;
 import com.kb.deposit.dto.DepositDTO;
 import org.apache.ibatis.annotations.Mapper;
 import java.util.List;
@@ -9,4 +10,6 @@ public interface DepositMapper {
     List<DepositDTO> getDepositList();
 
     void deleteDeposit(long id);
+
+    void insertDeposit(Deposit deposit);
 }
