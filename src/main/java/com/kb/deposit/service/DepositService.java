0772afd0d5541f8383b9735048004dade3373134
minju <betaa0528@gmail.com>
2024-10-14 19:05:05 +0900

    #86 - 선생님 페이지에서 은행관리하는 부분의 비즈니스 로직에서 예금을 등록하는 비즈니스 로직을 작성함 / 박민주

--- src/main/java/com/kb/deposit/service/DepositService.java
+++ src/main/java/com/kb/deposit/service/DepositService.java
@@ -1,5 +1,6 @@
 package com.kb.deposit.service;
 
+import com.kb.deposit.domain.Deposit;
 import com.kb.deposit.dto.DepositDTO;
 import com.kb.deposit.mapper.DepositMapper;
 import lombok.RequiredArgsConstructor;
@@ -19,5 +20,9 @@ public class DepositService {
 
     public void deleteDeposit(long id) {
         depositMapper.deleteDeposit(id);
+    }
+
+    public void addDeposit(Deposit deposit) {
+        depositMapper.insertDeposit(deposit);
     }
 }
