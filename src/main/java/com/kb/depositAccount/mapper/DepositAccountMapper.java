bc594480511b361797499309a70fb2a210a895a8
minju <betaa0528@gmail.com>
2024-10-15 09:39:48 +0900

    #90 - 예금, 적금가입에 필요한 매퍼들을 작성함 / 박민주

--- src/main/java/com/kb/depositAccount/mapper/DepositAccountMapper.java
+++ src/main/java/com/kb/depositAccount/mapper/DepositAccountMapper.java
@@ -1,9 +1,22 @@
 package com.kb.depositAccount.mapper;
 
+import com.kb.depositAccount.domain.DepositAccount;
 import com.kb.depositAccount.dto.DepositAccountDTO;
+import com.kb.depositAccount.dto.DepositAccountRequest;
+import com.kb.depositAccount.dto.DepositAccountResponse;
+import com.kb.member.dto.Member;
 import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
 
 @Mapper
 public interface DepositAccountMapper {
-    void insertDepositAccount(DepositAccountDTO depositAccountDTO);
+    int insertDepositAccount(DepositAccountRequest depositAccountRequest);
+
+
+    List<DepositAccountResponse> selectDepositAccount(long stdId);
+
+    int deleteDepositAccount(long id);
+
+    DepositAccount selectDepositAccountByAccountId(long id);
 }
