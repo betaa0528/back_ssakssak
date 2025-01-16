64d3e95ba4308616d633c25f82198c553bc2ff60
minju <betaa0528@gmail.com>
2025-01-16 22:46:22 +0900

    feat: batch로 주급을 자동지급하기 위한 쿼리와 지급한 기록을 남길 수 있는 쿼리를 새로 작성하였다.

--- src/main/java/com/kb/student/mapper/StudentMapper.java
+++ src/main/java/com/kb/student/mapper/StudentMapper.java
@@ -1,5 +1,6 @@
-    package com.kb.student.mapper;
+package com.kb.student.mapper;
 
+import com.kb.salary.dto.SalaryBatchRequest;
 import com.kb.student.domain.Student;
 import com.kb.student.dto.*;
 import lombok.extern.slf4j.Slf4j;
@@ -14,35 +15,49 @@ public interface StudentMapper {
     // 배치용
     List<StudentDTO> getStudentListByJobId();
 
+    void updateStudentSalaryBatch(SalaryBatchRequest salaryBatchRequest);
 
 
     // ================================
     List<StudentDTO> getStudentList();
+
     void insertStudent(StudentDTO studentDTO);
+
     // 학생 프로필 조회
     StudentDTO selectStudentProfile(Long studentId);
+
     List<DailyCheckDTO> selectRecentFiveDaysAttendance(@Param("studentId") Long studentId);
+
     int updateStudentSeed(@Param("stdId") long stdId, @Param("changeSeed") int changeSeed);
 
 
     //조은
     List<SeedRankingDTO> getSeedRanking();
+
     List<SeedRankingDTO> getSeedRankingThree();
 
     void insertCSVStudent(StudentCsvDTO student);
+
     List<StudentDTO> getAllStudents();
+
     List<SeedRankingDTO> getStudentSeed();
+
     int getStudentCount();
 
     void updateAllStudentSeed(int pay);
+
     void updateStudent(@Param("student") StudentDTO student);
-    void updateStudentJob(@Param("stdId") long stdId,@Param("jobId") long jobId);
+
+    void updateStudentJob(@Param("stdId") long stdId, @Param("jobId") long jobId);
 
     Student selectStudentById(long stdId);
-    Student selectStudentByUsernameAndStdName(@Param("username") String username,@Param("name") String name);
-    StudentDTO selectStudentByUsernameAndName(@Param("username") String username,@Param("name") String name);
+
+    Student selectStudentByUsernameAndStdName(@Param("username") String username, @Param("name") String name);
+
+    StudentDTO selectStudentByUsernameAndName(@Param("username") String username, @Param("name") String name);
 
     List<StudentSalaryDTO> selectStudentBaseSalary();
+
     List<StudentSalaryDTO> selectStudentAdditionalSalary();
 
 }
