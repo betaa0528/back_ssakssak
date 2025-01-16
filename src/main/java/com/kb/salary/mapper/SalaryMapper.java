4e3d08b71f92cd33e8571e338624bb3e8fbf4a2f
minju <betaa0528@gmail.com>
2025-01-16 22:46:09 +0900

    feat: batch로 주급을 자동지급하기 위한 쿼리와 지급한 기록을 남길 수 있는 쿼리를 새로 작성하였다.

--- src/main/java/com/kb/salary/mapper/SalaryMapper.java
+++ src/main/java/com/kb/salary/mapper/SalaryMapper.java
@@ -1,12 +1,18 @@
 package com.kb.salary.mapper;
 
 import com.kb.salary.dto.Salary;
+import com.kb.salary.dto.SalaryBatchRequest;
 import com.kb.salary.dto.SalaryHistory;
+import com.kb.student.dto.UpdatedStudentSalary;
 import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
 
 public interface SalaryMapper {
     void insertSalary(@Param("baseSalary") int baseSalary,@Param("additionalSalary") int additionalSalary);
-    Salary selectSalary();;
+    Salary selectSalary();
+    List<SalaryBatchRequest> selectSalaryByTchId();
+    void insertSalaryHistory(SalaryBatchRequest salaryBatchRequest);
 
 
 }
