963db7930d86ca402d9b5a883dbca8548d8e9b66
minju <betaa0528@gmail.com>
2025-03-24 23:16:10 +0900

    새롭게 리팩토링 하면서 예외를 Custom으로 처리하기로 하였다

--- src/main/java/com/kb/common/exception/GlobalExceptionHandler.java
+++ src/main/java/com/kb/common/exception/GlobalExceptionHandler.java
@@ -1,15 +1,29 @@
 package com.kb.common.exception;
 
-import lombok.extern.slf4j.Slf4j;
 import com.kb.member.exception.PasswordMissmatchException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-@RestControllerAdvice
 @Slf4j
-public class RestExceptionAdvice {
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(SsakssakApplicationException.class)
+    public ResponseEntity<String> handleIllegalArgumentException(IllegalArgumentException exception) {
+        return ResponseEntity.badRequest().body(exception.getMessage());
+    }
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<?> applicationHandler(RuntimeException e) {
+        log.error("Error occurs {}", e.getMessage());
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(ErrorResponse.error(ErrorCode.INTERNAL_ERROR.name()));
+    }
+
     @ExceptionHandler(PasswordMissmatchException.class)
     public ResponseEntity<?> handlePasswordError(Exception ex) {
         return ResponseEntity.status(400)
