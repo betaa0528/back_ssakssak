6a835e4fc5637b5ececd6ef5adfebaccf0bd8f26
minju <betaa0528@gmail.com>
2025-03-24 23:20:26 +0900

    Spring Boot로 마이그레이션 하면서 의존성이 변경되어 설정을 건드린 경우다

--- src/main/java/com/kb/stock/dto/StockTradeRequest.java
+++ src/main/java/com/kb/stock/dto/StockTradeRequest.java
@@ -1,12 +1,15 @@
 package com.kb.stock.dto;
 
-import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.PropertyNamingStrategy;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
 
-@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+import java.time.LocalDateTime;
+
+@JsonNaming(value = PropertyNamingStrategy.SnakeCaseStrategy.class)
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
