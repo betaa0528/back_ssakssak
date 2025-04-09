e280644e5b86b0fd9c517c942bdbdeb6e823f981
minju <betaa0528@gmail.com>
2025-04-09 09:09:36 +0900

    Feat: 에러코드에 관한 내용 추가

--- src/main/java/com/kb/common/exception/ErrorCode.java
+++ src/main/java/com/kb/common/exception/ErrorCode.java
@@ -9,7 +9,10 @@ import org.springframework.http.HttpStatus;
 public enum ErrorCode {
     INSUFFICIENT_SEED(HttpStatus.CONFLICT, "보유한 씨드가 부족합니다."),
     INSUFFICIENT_STOCK_QUANTITY(HttpStatus.CONFLICT, "잘못된 주식 수량입니다."),
+    INSUFFICIENT_AMOUNT(HttpStatus.CONFLICT, "잘못된 수입/지출 금액 입력"),
+    USER_NOT_FOUND(HttpStatus.NOT_FOUND, "유효하지 않은 학생 ID 입니다."),
     INTERNAL_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "Internal error")
+
 
     ;
 
